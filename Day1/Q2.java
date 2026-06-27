e86919b346b6e121d787c8f0bc39e2b8fccd6bfe
Voidwalker-AT <anmol392005@gmail.com>
2026-06-27 21:26:54 +0530

    Day1

---
+++ Day1/Q2.java
@@ -0,0 +1,18 @@
+class Solution
+{
+    public int[] twoSum(int[] nums, int a)
+    {
+        int i,j;
+        for(i=0;i<=nums.length;i++)
+        {
+            for(j=i+1;j<nums.length;j++)
+            {
+                if(nums[i]+nums[j]==a)
+                {
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[] {};
+    }
+}
